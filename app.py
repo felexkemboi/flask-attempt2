d12d2309e723b56bf5809949f6279f11685593d5
felexkemboi <felokemboi10@gmail.com>
2017-11-13 11:03:31 +0300

    add a sign up method to handle requests

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return "Welcome to Flask Framework!!"
+
+@app.route('/signUp')
+def signUp():
+    return render_template('signUp.html')
 
 if __name__ == '__main__':
 	app.run()
